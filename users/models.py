a6a67cab95e964284eee799a1f4d9a03fd1db7a2
Moses Korir <moscoluiz11@gmail.com>
2020-05-20 19:59:16 +0300

    Bio & Location changes

--- users/models.py
+++ users/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    bio = models.CharField(max_length=100, blank=True)
-    location = models.CharField(max_length=30, blank=True)
+    bio = models.CharField(max_length=100, blank=True, null=True)
+    location = models.CharField(max_length=30, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
